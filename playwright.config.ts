20be844d3e35549b75c65803d88312b1e2743a97
Pavan Kumar Talari <1696@DOMAIN.APCFSS.IN>
2026-09-03 11:21:19 +0530

    updated config

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: process.env.BASE_URL,
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    video: 'off',
 
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
